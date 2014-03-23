13398b60fed52380f060bc1cf91756996606413a
Tyler Peterson <petersontb@familysearch.org>
2014-03-22 21:25:12 -0600

    Return listing of files
    Return a listing of files so that users of the library can serve them
    up for their pages.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = {};
+// Return a mapping of the crucial files and their paths on disk
+
+var path = require('path'),
+    files = {};
+
+function addPath(map, filename) {
+  map[filename] = path.join(__dirname, filename);
+}
+
+addPath(files, 'flexgrid.css');
+addPath(files, 'flexgrid-ie.css');
+addPath(files, 'modernizr.js');
+addPath(files, 'respond.js');
+
+module.exports = files;
+
+if (require.main === module) {
+  console.log(module.exports);
+}
